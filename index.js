c1ccd16724ce4de94d357cce479991206a656816
Masum-Arifin <masumarifinbd@gmail.com>
2022-09-30 01:49:09 +0600

    users added

--- index.js
+++ index.js
@@ -43,21 +43,7 @@ const addUser = () =>{
 // CREATING REDUCER
 const userReducer = (state = initialState, action) => {
   switch (action.type) {
-    case INCREMENT:
-      return {
-        ...state,
-        count: state.count + 1,
-      };
-    case DECREMENT:
-      return {
-        ...state,
-        count: state.count - 1,
-      };
-    case RESET:
-      return {
-        ...state,
-        count: 0,
-      };
+    
     case INCREMENT_BY_VALUE:
       return {
         ...state,
